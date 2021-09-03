28159f698992d1c95c05a5e913b3d4ba2eca8755
Tancre <outisisout@gmail.com>
2021-09-03 17:43:01 +0200

    minimap + style.css

--- assets/js/modules/Setup.js
+++ assets/js/modules/Setup.js
@@ -1,5 +1,7 @@
 function setup() {
   noCanvas();
+  minimapInit();
+  
   const textArea = select('#textArea');
   const submit = select('#submit');
 
@@ -46,6 +48,21 @@ function dropFile(area) {
     const textArea = select('#textArea');
     textArea.value(file.data);
   }
+}
+
+function minimapInit() {
+  pagemap(document.querySelector('#map'), {
+    viewport: null,
+    styles: {
+        'header,footer,section,article': 'rgba(0,0,0,0.08)',
+        'h1,a': 'rgba(0,0,0,0.10)',
+        'h2,h3,h4,span': 'red'
+    },
+    back: 'rgba(0,0,0,0.02)',
+    view: 'rgba(0,0,0,0.05)',
+    drag: 'rgba(0,0,0,0.10)',
+    interval: null
+  });
 }
 
 
