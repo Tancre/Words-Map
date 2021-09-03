1ff6d1bd5c8ed9b8eb844b976ebce9b5ae558d1a
Tancre <outisisout@gmail.com>
2021-09-03 17:47:52 +0200

    update minimap + del elm on cancel key

--- assets/js/modules/Events.js
+++ assets/js/modules/Events.js
@@ -1,3 +1,20 @@
+
+// Update minimap on changes
+window.addEventListener('keyup', () => minimapInit());
+window.addEventListener('mouseup', () => minimapInit());
+
+// const miniMap = document.querySelector('#map');
+// miniMap.addEventListener('dragstart', () => {
+//     const dragSelection = document.querySelector('.ui-selectable-helper');
+//     dragSelection.classList.add("no-outline");
+// })
+
+// miniMap.addEventListener('mouseup', () => {
+//     const dragSelection = document.querySelector('.ui-selectable-helper');
+//     dragSelection.classList.remove("no-outline");
+// })
+
+// Toggle Regex Input
 $(document).on('change', '#selectValue', () => {
     const selected = document.querySelector('select').value;
     if (selected == 4) {
@@ -7,10 +24,18 @@ $(document).on('change', '#selectValue', () => {
     }
 });
 
+// Delete elements
 function deleteElm() {
+    // Double click
     const spans = document.querySelectorAll('span');
-    spans.forEach(span => {
-        span.addEventListener("dblclick", event => event.target.remove());
-    })
-}
+    spans.forEach(span => span.addEventListener("dblclick", event => event.target.remove()));
 
+    // Delete key
+    $(document).keydown(function( event ) {
+        if ( event.which == 46 ) {
+            event.preventDefault();
+            const selected = document.querySelectorAll('.ui-selected');
+            selected.forEach(elt => elt.remove());
+        }
+    });
+}
