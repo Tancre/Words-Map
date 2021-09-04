151630d927dfe7c30299c850ffd6fd7311de23e3
Tancre <outisisout@gmail.com>
2021-09-04 18:17:59 +0200

    cleaned a bit and activate resize

--- assets/js/modules/DragResize.js
+++ assets/js/modules/DragResize.js
@@ -5,8 +5,7 @@ class DragResize {
         this.setZindex();
         this.handleCursor();
         this.multidrag();
-        //this.test();
-        //this.resize();
+        this.resize();
     }
 
     drag() {
@@ -39,51 +38,4 @@ class DragResize {
         $(".resizable").resizable({ aspectRatio: true, maxHeight: 900, minHeight: 10 });
         $(".resizableOriz").resizable({ aspectRatio: true, maxHeight: 500, minHeight: 10 });
     }
-
-    test() {
-        function detectspecialkeys(e) {
-            var evtobj = window.event ? event : e
-            if (evtobj.altKey || evtobj.ctrlKey || evtobj.shiftKey)
-                alert("you pressed one of the 'Alt', 'Ctrl', or 'Shift' keys")
-        }
-        document.onkeypress = detectspecialkeys();
-        $('.draggable')
-            .click(function (e) {
-                if (e.ctrlKey) {
-                    $(this).toggleClass("selected");
-                }
-            })
-    }
 }
-
-// function draggable() {
-//     let a = 3;
-//     drag(a);
-//     setZindex(a);
-//     handleCursor();
-// }
-
-// function drag(a) {
-//     $('.draggable').draggable({ containment: "window" }, {            // Setup dragging and containment
-//         start: (event, ui) => $(this).css("z-index", a++)    // Initialize z-index for handelr
-//     });
-// }
-
-// function setZindex(a) {
-//     $('#dragZone div').click(() => {                              // Setup z-index handler
-//         $(this).addClass('top').removeClass('bottom');
-//         $(this).siblings().removeClass('top').addClass('bottom');
-//         $(this).css("z-index", a++);
-//     });
-// }
-
-// function handleCursor() {
-//     $('.draggable')
-//         .on("mousedown", () => $('.draggable').css('cursor', 'grabbing'))
-//         .on("mouseup mouseleave", () => $('.draggable').css('cursor', 'grab'));
-// }
-
-// function resize() {
-//     $(".resizable").resizable({ aspectRatio: true, maxHeight: 900, minHeight: 10 });
-//     $(".resizableOriz").resizable({ aspectRatio: true, maxHeight: 500, minHeight: 10 });
-// }
